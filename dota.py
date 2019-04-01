2f4f59c87039555bf06a66055af12c2afe234f1a
MajdVictor <36989426+MajdVictor@users.noreply.github.com>
2019-04-01 17:38:39 +0300

    Update dota.py

--- dota.py
+++ dota.py
@@ -5,6 +5,6 @@ http = urllib3.PoolManager()
 
 heroes = http.request('GET','https://api.opendota.com/api/heroes')
 heroes_dict = json.loads(heroes.data.decode('UTF-8'))
-
+##printing out the localized name of every hereo 
 for hero in heroes_dict:
     print(hero['localized_name'])
